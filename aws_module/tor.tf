8a51c6098675b2201bc1e6d4800a7351334cf5ae
sgasimov-dotcom <sahib.gasimov2@gmail.com>
2021-07-22 20:16:37 +0000

    added git repo source with comment

--- aws_module/tor.tf
+++ aws_module/tor.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 module "vpc_dev" {
   source            = "../aws_instance"
+ //source               = "git@github.com:sgasimov-dotcom/interactive-learning-tasks.git//aws_instance"
   key_name          = "class2"
   public_key = "~/.ssh/id_rsa.pub"
   instance_type     = "t3.micro"
