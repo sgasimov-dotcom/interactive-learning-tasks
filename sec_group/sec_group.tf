9209aab817921567df9a1f71fc9c50bf1f494fad
sgasimov-dotcom <84157053+sgasimov-dotcom@users.noreply.github.com>
2021-07-29 01:44:05 -0500

    sadsad

---
+++ sec_group/sec_group.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "sec_group" {
+  name        = "sec_group"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["50.194.68.230/32"]
+  }
+
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["50.194.68.230/32"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
+
